38b0994400b716e1e2e4dd87d680cd6d31ee60d1
Joy <joymasih43@gmail.com>
2023-12-12 15:20:47 +0530

    added local storage

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const emailForm = document.getElementById('emailForm');
 
 emailForm.addEventListener('submit', function(e){
     e.preventDefault()
-    const email = document.getElementById('email').value
-    console.log(email);
+    const email = document.getElementById('email')
+    localStorage.setItem('email', email.value);
+    window.location.href = 'success.html'
 })
+
+    // console.log(email);
